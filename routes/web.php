3c9aaf750e7a16861db921a2cea70b05c49a9435
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2025-01-02 21:05:14 -0300

    feat: replace welcome view & add ArticleController routes

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,18 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ArticleController::class, 'index'])->name('articles.index');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::resource('articles', ArticleController::class)->except('index');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
