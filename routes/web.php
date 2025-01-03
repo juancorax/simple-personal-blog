f92fd8802cb9b4d2c2d874594571c1e3e490ec37
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2025-01-03 11:43:29 -0300

    fix: redirect '/' to '/articles'

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,8 @@ use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [ArticleController::class, 'index'])->name('articles.index');
+Route::redirect('/', '/articles');
+Route::get('/articles', [ArticleController::class, 'index'])->name('articles.index');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
