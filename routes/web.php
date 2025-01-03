22abd296ca37723aa922aa15917857183c7c2146
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2025-01-03 11:58:18 -0300

    fix: replace dashboard with articles index
    remove welcome view

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,6 @@ use Illuminate\Support\Facades\Route;
 Route::redirect('/', '/articles');
 Route::get('/articles', [ArticleController::class, 'index'])->name('articles.index');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::resource('articles', ArticleController::class)->except('index');
 
